aa6ebad532030627cb35aa668c3e3bcab6295c0a
geoffrey <geoffrey.waiganjo@student.moringaschool.com>
2024-08-28 11:03:19 +0300

    lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    '''Prints the Fibonacci sequence up to the given length.'''
+    fib_sequence = []
+    a, b = 0, 1
+    for _ in range(length):
+        fib_sequence.append(a)
+        a, b = b, a + b
+    print(fib_sequence)
+   
